8ad7adef9024e1453bdc01d638997701ac9a0282
Shameel Abdul <shameelm@skillspire.net>
2022-05-03 16:13:46 -0500

    React Lesson April 30 2022

--- src/components/content.js
+++ src/components/content.js
@@ -1,9 +1,61 @@
+import { useState } from "react";
 import StudentList from "./studentList";
 
 const Content = () => {
+    const [students, setStudents] = useState(        
+        [
+        {
+            "id" : 1,
+            "Header": "Musk Bought Tesla",
+            "lastName": "Abdul",
+            "course": "MERN"
+        },
+        {
+            "id" : 2,
+            "firstName": "Jack",
+            "lastName": "Sparrow",
+            "course": "Python"
+        },
+        {
+            "id" : 3,
+            "firstName": "Ricky",
+            "lastName": "Ponting",
+            "course": "MERN"
+        },
+        {
+            "id" : 4,
+            "firstName": "Watson",
+            "lastName": "Daniel",
+            "course": "PYTHON"
+        },
+        {
+            "id" : 5,
+            "firstName": "Kayla",
+            "lastName": "Mccausson",
+            "course": "MERN"
+        }
+    ]);
+
+    const [searchText, setSearchText] = useState("");
+
+    const deleteHandler = (id) => {
+        const list = students.filter(student => student.id != id);
+        setStudents(list);
+    };
+
     return (
         <div>
-            <StudentList></StudentList>
+            <StudentList students={students} header="All Students" deleteHandler={deleteHandler}></StudentList>
+            <br/>
+            <label>Search : </label>
+            <input
+            name='searchText'
+            id='searchText'
+            type='text'
+            value={searchText}
+            onChange={e => setSearchText(e.target.value) }
+            ></input>
+            {searchText && <StudentList students={students.filter((student) => student.course.toLowerCase() === searchText.toLowerCase())} header= {"Search Result - " + searchText} deleteHandler={deleteHandler}></StudentList>}
         </div>
     );
 }
--- src/components/studentList.js
+++ src/components/studentList.js
@@ -1,32 +1,12 @@
-const list =
-[
-    {
-        "id" : 1,
-        "firstName": "Shameel",
-        "lastName": "Abdul",
-        "course": "MERN"
-    },
-    {
-        "id" : 2,
-        "firstName": "Jack",
-        "lastName": "Sparrow",
-        "course": "Python"
-    },
-    {
-        "id" : 3,
-        "firstName": "Ricky",
-        "lastName": "Ponting",
-        "course": "MERN"
-    }
-];
-
-const StudentList = () => {
+const StudentList = ({students, header, deleteHandler}) => {
     return (
         <div className="student-list">
-            {list.map(student => (
-                <div class="student-preview">
+            <h2 style={{"padding": "20px"}}>{header}</h2>
+            {students.map(student => (
+                <div key={student.id} className="student-preview">
                     <h2>{student.firstName} {student.lastName}</h2>
                     <p>Enrolled to {student.course}</p>
+                    <button onClick={() => deleteHandler(student.id)}>Delete</button>
                 </div>
             ))}
         </div>
